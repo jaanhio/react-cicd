31635603c54a6cafd137e16d4ee2510b87de7a74
jaanhio <jianhaot.89@gmail.com>
2018-04-30 10:53:29 +0800

    added routings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import Contact from './Contact';
+import Home from './Home';
+import About from './About';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +17,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/contact" exact component={Contact} />
+          </Switch>
+        </div>
       </div>
     );
   }
