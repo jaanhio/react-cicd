2cff30f251f50bee1c27ed6f9482c9ed47ce2422
jaanhio <jianhaot.89@gmail.com>
2018-04-30 11:37:04 +0800

    fetching data

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div>
-      <p>this is the Home page</p>
-    </div>
-  );
+class Home extends Component {
+  state = {
+    awardsList: null
+  }
+
+  componentDidMount() {
+    axios.get('https://ollida-api.herokuapp.com/api/v1/awards').then(res => {
+      const { data } = res;
+      console.log(data);
+      this.setState({
+        awardsList: data
+      });
+    });
+  }
+
+  render() {
+    const renderAwards = this.state.awardsList && this.state.awardsList.map((awards, index) => {
+      return (
+        <p>{awards.name}</p>
+      )
+    });
+    if (this.state.awardsList) {
+      return (
+        <div>
+          {renderAwards}
+        </div>
+      );
+    }
+    else {
+      return (
+        <div>
+          <p>fetching data</p>
+        </div>
+      )
+    }
+  }
 }
-
 export default Home;
